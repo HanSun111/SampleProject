889a89f5ae56fe045f80ef1b53be8f4b00266426
hansun111 <hsun7920@bths.edu>
2022-09-20 20:12:27 -0400

    i added food print

--- src/Sample.java
+++ src/Sample.java
@@ -1,7 +1,7 @@
-import java.util.Scanner;
 public class Sample {
     public static void main(String[] args) {
         System.out.println("Han Sun");
-        System.out.print("Interstellar");
+        System.out.println("Interstellar");
+        System.out.println("I like fired chicken from PopEye");
     }
 }
